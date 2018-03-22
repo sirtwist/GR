4d94d55aea3bb3b4a875c4b0e2fcae80bcea7cec
George Roberts <groberts@moddigital.net>
2018-03-22 13:33:12 -0500

    Additional tests
    Add additional tests and update one test to use Assert.ThrowsException

--- GR.Tests/Tests.cs
+++ GR.Tests/Tests.cs
@@ -59,7 +59,6 @@ namespace GR.Tests
         }
 
         [TestMethod]
-        [ExpectedException(typeof(FormatException), "An improperly formatted string was allowed.")]
         public void Input_UnknownLineFormat()
         {
             // Arrange
@@ -69,9 +68,107 @@ namespace GR.Tests
 
             // Act
 
-            Person actual = Input.ParseLine(input);
 
             // Assert
+            Assert.ThrowsException<FormatException>(() => Input.ParseLine(input));
+        }
+
+        [TestMethod]
+        public void Person_BlankFirstName()
+        {
+            // Arrange
+            string input = "Roberts, , Male, Purple, 1973-04-11";
+            Person expected = new Person() { LastName = "Roberts", FirstName = "George", Gender = Gender.Male, FavoriteColor = "Purple", DateOfBirth = new DateTime(1973, 4, 11) };
+
+            // Act
+
+            // Assert
+
+            Assert.ThrowsException<FormatException>(() => Input.ParseLine(input));
+        }
+
+        [TestMethod]
+        public void Person_BlankLastName()
+        {
+            // Arrange
+            string input = ", George, Male, Purple, 1973-04-11";
+            Person expected = new Person() { LastName = "Roberts", FirstName = "George", Gender = Gender.Male, FavoriteColor = "Purple", DateOfBirth = new DateTime(1973, 4, 11) };
+
+            // Act
+
+            // Assert
+
+            Assert.ThrowsException<FormatException>(() => Input.ParseLine(input));
+        }
+
+        [TestMethod]
+        public void Person_BlankColor()
+        {
+            // Arrange
+            string input = "Roberts, George, Male, , 1973-04-11";
+            Person expected = new Person() { LastName = "Roberts", FirstName = "George", Gender = Gender.Male, FavoriteColor = "Purple", DateOfBirth = new DateTime(1973, 4, 11) };
+
+            // Act
+
+            // Assert
+
+            Assert.ThrowsException<FormatException>(() => Input.ParseLine(input));
+        }
+
+        [TestMethod]
+        public void Person_BlankGender()
+        {
+            // Arrange
+            string input = "Roberts, George, Male, , 1973-04-11";
+            Person expected = new Person() { LastName = "Roberts", FirstName = "George", Gender = Gender.Male, FavoriteColor = "Purple", DateOfBirth = new DateTime(1973, 4, 11) };
+
+            // Act
+
+            // Assert
+
+            Assert.ThrowsException<FormatException>(() => Input.ParseLine(input));
+        }
+
+        [TestMethod]
+        public void Person_BlankBirthdate()
+        {
+            // Arrange
+            string input = "Roberts, George, Male, Purple, ";
+            Person expected = new Person() { LastName = "Roberts", FirstName = "George", Gender = Gender.Male, FavoriteColor = "Purple", DateOfBirth = new DateTime(1973, 4, 11) };
+
+            // Act
+
+            // Assert
+
+            Assert.ThrowsException<FormatException>(() => Input.ParseLine(input));
+        }
+
+        [TestMethod]
+        public void Person_InvalidGender()
+        {
+            // Arrange
+            string input = "Roberts, George, Human, Purple, 1973-04-11";
+            Person expected = new Person() { LastName = "Roberts", FirstName = "George", Gender = Gender.Male, FavoriteColor = "Purple", DateOfBirth = new DateTime(1973, 4, 11) };
+
+            // Act
+
+            // Assert
+
+            Assert.ThrowsException<FormatException>(() => Input.ParseLine(input));
+        }
+
+        [TestMethod]
+        public void Person_InvalidDate()
+        {
+            // Arrange
+            string input = "Roberts, George, Male, Purple, 1973-44-11";
+            Person expected = new Person() { LastName = "Roberts", FirstName = "George", Gender = Gender.Male, FavoriteColor = "Purple", DateOfBirth = new DateTime(1973, 4, 11) };
+
+            // Act
+
+            // Assert
+
+            Assert.ThrowsException<FormatException>(() => Input.ParseLine(input));
         }
 
     }
