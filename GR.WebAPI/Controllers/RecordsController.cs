6f63dce7534e34bb9c41e68b2fc29969af912853
George Roberts <groberts@moddigital.net>
2018-03-22 23:55:23 -0500

    Add web service
    Add web service to use same business logic

---
+++ GR.WebAPI/Controllers/RecordsController.cs
@@ -0,0 +1,59 @@
+﻿using GR.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Caching;
+using System.Web.Http;
+
+namespace GR.WebAPI.Controllers
+{
+    public class RecordsController : ApiController
+    {
+
+
+        [HttpGet]
+        [Route("records/{sort}")]
+        public IEnumerable<Record> Get(string sort)
+        {
+            OutputType outputType = OutputType.Gender;
+            switch (sort.ToLower())
+            {
+                case "gender":
+                    outputType = OutputType.Gender;
+                    break;
+                case "birthdate":
+                    outputType = OutputType.Birthdate;
+                    break;
+                case "name":
+                    outputType = OutputType.Lastname;
+                    break;
+            }
+            List<Record> people = new List<Record>();
+            if (MemoryCache.Default.Contains("People"))
+            {
+                people = (List<Record>)MemoryCache.Default["People"];
+            }
+            return Output.Sort(people, outputType);
+        }
+
+
+        // POST: api/Records
+        [HttpPost]
+        [Route("records")]
+        public void Post([FromBody]string value)
+        {
+            List<Record> people = new List<Record>();
+            if (MemoryCache.Default.Contains("People"))
+            {
+                people = (List<Record>)MemoryCache.Default["People"];
+            }
+            var factory = new PersonFactory();
+            people.Add(Input.ParseLine(factory, value));
+
+            MemoryCache.Default.Set("People", people, DateTimeOffset.Now.AddDays(30));
+        }
+
+    }
+}
